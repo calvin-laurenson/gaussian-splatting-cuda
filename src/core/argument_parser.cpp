b5abb251954d4d60895e3069441efe6be59475f7
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2025-09-01 10:26:53 -0700

    ply initialization

--- src/core/argument_parser.cpp
+++ src/core/argument_parser.cpp
@@ -95,6 +95,7 @@ namespace {
             ::args::ValueFlag<float> init_extent(parser, "init_extent", "Extent of random initialization", {"init-extent"});
             ::args::ValueFlagList<std::string> timelapse_images(parser, "timelapse_images", "Image filenames to render timelapse images for", {"timelapse-images"});
             ::args::ValueFlag<int> timelapse_every(parser, "timelapse_every", "Render timelapse image every N iterations (default: 50)", {"timelapse-every"});
+            ::args::ValueFlag<std::string> init_ply(parser, "init_ply", "Optional PLY splat file for initialization", {"init-ply"});
 
             // Logging options
             ::args::ValueFlag<std::string> log_level(parser, "level", "Log level: trace, debug, info, warn, error, critical, off (default: info)", {"log-level"});
@@ -183,6 +184,16 @@ namespace {
                 }
 
                 return std::make_tuple(ParseResult::Success, std::function<void()>{});
+            }
+
+            if (init_ply) {
+                const auto ply_path = ::args::get(init_ply);
+                params.init_ply = ply_path;
+
+                // Check if PLY file exists
+                if (!std::filesystem::exists(ply_path)) {
+                    return std::unexpected(std::format("Initialization PLY file does not exist: {}", ply_path));
+                }
             }
 
             // Training mode
